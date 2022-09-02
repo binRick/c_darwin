6cfc3cfc46195362687bdd94e2401794a447101b
rick <rick@ricks-MacBook-Air.local>
2022-09-02 16:04:33 -0400

    refactor utils

--- core-utils/core-utils.h
+++ core-utils/core-utils.h
@@ -12,10 +12,16 @@
 #include <stdint.h>
 #include <stdio.h>
 //////////////////////////////////////////////////////////////
+#include "app-utils/app-utils.h"
 #include "core-utils/core-utils-extern.h"
 #include "display-utils/display-utils.h"
 #include "dock-utils/dock-utils.h"
+#include "image-utils/image-utils.h"
+#include "menu-bar-utils/menu-bar-utils.h"
+#include "mission-control-utils/mission-control-utils.h"
+#include "process-utils/process-utils.h"
 #include "process/process.h"
 #include "system-utils/system-utils.h"
+#include "table-utils/table-utils.h"
 #include "window-utils/window-utils.h"
 //////////////////////////////////////////////////////////////
